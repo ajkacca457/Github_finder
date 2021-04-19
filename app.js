1a827998e34058f26e8ef6e83c986caf02df6145
Avijit Karmaker <ajkacca@gmail.com>
2021-04-19 21:50:23 +0300

    add all get functionalities

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const search=document.querySelector("#search");
 const sbtn=document.querySelector("#sbtn");
 const alert=document.querySelector(".alert");
+const output=document.querySelector(".output");
 
 sbtn.addEventListener("click",(e)=>{
   e.preventDefault();
@@ -19,6 +20,24 @@ else {
 
 
 const displayUser=(data)=>{
+output.classList.add("show");
+if(data.message){
+output.innerHTML=`<h4>${data.message}</h4>`
+}else {
+  output.innerHTML=`<h4>${data.name}</h4>
+  <img src="${data.avatar_url}" alt="${data.name} info" class="proimg">
+  <p class="w-75 mx-auto">${data.bio}</p>
+  <p>${data.company}</p>
+  <p><i class="fas fa-globe mr-2"></i> portfolio/Website: <a href="${data.blog}" class="text-white">Go to website</a></p>
+  <div class="info">
+  <p><i class="fas fa-users"></i> Number of Followers: ${data.followers}</p>
+  <p><i class="fas fa-eye"></i> Following: ${data.following}</p>
+  <p><i class="fas fa-code-branch"></i> Number of Repos: ${data.public_repos}</p>
+  </div>
+  <p><i class="fas fa-compass mr-2"></i> Location: ${data.location}</p>
+  <p><i class="fab fa-twitter"></i> ${data.twitter_username}</p>
+  `
+}
 console.log(data);
 }
 
